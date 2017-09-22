402e7e98148876957daccfb2cc46efd78d1b31fb
m-nino14 <masato67@gmail.com>
2017-09-22 14:11:12 +0900

    バナー追加

--- biz/workstyle_henkaku/common/js/fund.js
+++ biz/workstyle_henkaku/common/js/fund.js
@@ -1,15 +1,34 @@
 $(function() {
 	function init(){
+		var trackList = new Array();
+		var num;
+		trackList[1] = "slTrackEvent(['ms','mslink','foot_01_fws'])";
+		trackList[2] = "slTrackEvent(['ms','mslink','foot_02_tp'])";
+		trackList[3] = "slTrackEvent(['ms','mslink','foot_03_intel'])";
+		trackList[4] = "slTrackEvent(['ms','mslink','foot_04_fsl'])";
+		trackList[5] = "slTrackEvent(['ms','mslink','foot_05_fmw'])";
+		
+		/*$.fn.trackAct = function(num){
+			trackList[num];	
+		};*/
+						
 		$('main .btnList').html(
 		'<div class="inner">\
 			<ul>\
-				<li><a href="http://www.fujitsu.com/jp/innovation/workstyle"><div class="change-border01"><div class="change-border01__inner"><img src="/biz/workstyle_henkaku/images/btn4.jpg" class="pc-only" alt="富士通のワークスタイル変革"></div></div><img src="/biz/workstyle_henkaku/images/sp/btn4.jpg" class="sp-only" alt="富士通のワークスタイル変革"><span>富士通の<br>ワークスタイル変革サイト</span></a></li>\
-				<li><a href="http://transformation.ismedia.jp/"><div class="change-border01"><div class="change-border01__inner"><img src="/biz/workstyle_henkaku/images/btn3.jpg" class="pc-only" alt="新・働き方総合研究所"></div></div><img src="/biz/workstyle_henkaku/images/sp/btn3.jpg" class="sp-only" alt="新・働き方総合研究所"><span>ワークスタイル変革の<br>テーマポータルサイト</span></a></li>\
-				<li><a href="http://www.fmworld.net/biz/security_lab/"><div class="change-border01"><div class="change-border01__inner"><img src="/biz/workstyle_henkaku/images/btn2.jpg" class="pc-only" alt="FUJITSU PC Security Lab."></div></div><img src="/biz/workstyle_henkaku/images/sp/btn2.jpg" class="sp-only" alt="FUJITSU PC Security Lab."><span>認証セキュリティなら<br>富士通</span></a></li>\
-				<li><a href="http://www.fmworld.net/biz/fmv/"><div class="change-border01"><div class="change-border01__inner"><img src="/biz/workstyle_henkaku/images/btn1.jpg" class="pc-only" alt="FMworld[法人]"></div></div><img src="/biz/workstyle_henkaku/images/sp/btn1.jpg" class="sp-only" alt="FMworld[法人]"><span>富士通製品の<br>ご紹介</span></a></li>\
+				<li><a href="http://www.fujitsu.com/jp/innovation/workstyle" target="_blank" onclick="$(this).trackAct(1);"><div class="change-border01"><div class="change-border01__inner"><img src="/biz/workstyle_henkaku/images/btn4.jpg" class="pc-only" alt="富士通のワークスタイル変革"></div></div><img src="/biz/workstyle_henkaku/images/sp/btn4.jpg" class="sp-only" alt="富士通のワークスタイル変革"><span>富士通の<br>ワークスタイル変革サイト</span></a></li>\
+				<li><a href="http://transformation.ismedia.jp/" target="_blank" onclick="$(this).trackAct(2);"><div class="change-border01"><div class="change-border01__inner"><img src="/biz/workstyle_henkaku/images/btn3.jpg" class="pc-only" alt="新・働き方総合研究所"></div></div><img src="/biz/workstyle_henkaku/images/sp/btn3.jpg" class="sp-only" alt="新・働き方総合研究所"><span>ワークスタイル変革の<br>テーマポータルサイト</span></a></li>\
+				<li><a href="https://www.intel.co.jp/content/www/jp/ja/small-business/pc-refresh.html" target="_blank" onclick="$(this).trackAct(3);"><div class="change-border01"><div class="change-border01__inner"><img src="/biz/workstyle_henkaku/images/btn5.jpg" class="pc-only" alt="intel ワークスタイル変革！従業員をオフィスから解放しませんか？"></div></div><img src="/biz/workstyle_henkaku/images/sp/btn5.jpg" class="sp-only" alt="intel ワークスタイル変革！従業員をオフィスから解放しませんか？"><span>インテルのテクノロジーで、<br>働き改革を実現</span></a></li>\
+				<li><a href="http://www.fmworld.net/biz/security_lab/" target="_blank" onclick="$(this).trackAct(4);"><div class="change-border01"><div class="change-border01__inner"><img src="/biz/workstyle_henkaku/images/btn2.jpg" class="pc-only" alt="FUJITSU PC Security Lab."></div></div><img src="/biz/workstyle_henkaku/images/sp/btn2.jpg" class="sp-only" alt="FUJITSU PC Security Lab."><span>認証セキュリティなら富士通</span></a></li>\
+				<li><a href="http://www.fmworld.net/biz/fmv/" target="_blank" onclick="$(this).trackAct(5);"><div class="change-border01"><div class="change-border01__inner"><img src="/biz/workstyle_henkaku/images/btn1.jpg" class="pc-only" alt="FMworld[法人]"></div></div><img src="/biz/workstyle_henkaku/images/sp/btn1.jpg" class="sp-only" alt="FMworld[法人]"><span>富士通製品のご紹介</span></a></li>\
 			</ul>\
 		</div>'
-		);	
+		);
+		
+		$('main .btnList li:nth-child(1) a').attr("onclick",trackList[1]);
+		$('main .btnList li:nth-child(2) a').attr("onclick",trackList[2]);
+		$('main .btnList li:nth-child(3) a').attr("onclick",trackList[3]);
+		$('main .btnList li:nth-child(4) a').attr("onclick",trackList[4]);
+		$('main .btnList li:nth-child(5) a').attr("onclick",trackList[5]);
 		
 		$('#fm_fund').html(
 		'<div class="inner">\
